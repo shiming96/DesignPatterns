fbe5025152ee3a83a9c6f548f89aef806049d6c4
ShiMing <mingshi_sm@163.com>
2019-01-14 14:24:53 +0800

    add strategy

---
+++ strategy/src/MiniDuckSimulator1.java
@@ -0,0 +1,15 @@
+public class MiniDuckSimulator1 {
+ 
+	public static void main(String[] args) {
+ 
+		Duck mallard = new MallardDuck();
+		mallard.performQuack();
+		mallard.performFly();
+   
+		Duck model = new ModelDuck();
+		model.performFly();
+		model.setFlyBehavior(new FlyRocketPowered());
+		model.performFly();
+
+	}
+}
---
+++ strategy/src/RubberDuck.java
@@ -0,0 +1,11 @@
+public class RubberDuck extends Duck {
+ 
+	public RubberDuck() {
+		flyBehavior = new FlyNoWay();
+		quackBehavior = new Squeak();
+	}
+ 
+	public void display() {
+		System.out.println("I'm a rubber duckie");
+	}
+}
