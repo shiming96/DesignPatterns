e0b1dd9b0e71a7c3fe7e8adbd860e5911441e604
ShiMing <mingshi_sm@163.com>
2019-01-15 09:32:26 +0800

    add singleton

--- decorator/src/io/LowerCaseInputStream.java
+++ decorator/src/io/LowerCaseInputStream.java
@@ -1,5 +1,9 @@
 package io;
 
+import java.io.FilterInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class LowerCaseInputStream extends FilterInputStream {
 
 	public LowerCaseInputStream(InputStream in) {
